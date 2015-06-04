16d721ecd808acbefce39de5a1295c7c20bc1ec0
Rasmus <rasmus.webs@gmail.com>
2015-06-04 11:05:35 +0200

    Timer

--- src/com/pungdjurProductions/Main.java
+++ src/com/pungdjurProductions/Main.java
@@ -7,7 +7,9 @@ public class Main {
 
     public static void main(String[] args) {
 
-        new TimeStep();
+        TimeStep t = new TimeStep();
+        
+        t.start();
 
     }
 }
--- src/com/pungdjurProductions/physics/TimeStep.java
+++ src/com/pungdjurProductions/physics/TimeStep.java
@@ -1,5 +1,13 @@
 package com.pungdjurProductions.physics;
 
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
+import java.util.LinkedList;
+
+import javax.swing.JFrame;
+
+
+
 /**
  * Created by Rasmus on 2015-06-04.
  */
@@ -7,18 +15,51 @@ public class TimeStep {
     final float fps = 100;
     final float dt = 1 / fps;
     float accumulator = 0;
-    MyFrame myFrame;
+    LinkedList<Object> olist;
+    JFrame frame;
 
     public TimeStep(){
-        myFrame = new MyFrame();
-        start();
+        frame = new JFrame();
+        init();
+       
+    }
+    
+    public void init(){
+    	olist = new LinkedList<Object>();
+    	
+    	olist.add(new Rect(50, 50, 30, 30, 1, 1));
+    	
+    	
+    }
+    
+    public void renderGraphics(BufferStrategy b) {
+		Graphics g = b.getDrawGraphics();
+		frame.requestFocus();
+		
+		for(Object o : olist){
+    		o.render(g);
+    	}
+
+		
+		b.show();
+		g.dispose();
+
+	}
+    
+    private void update(double dt){
+    	for(Object o : olist){
+    		o.update(dt);
+    	}
     }
 
-    private void start() {
+
+    public void start() {
+    	
+    	
         // In units seconds
         float frameStart = System.currentTimeMillis();
 
-// main loop
+        // main loop
         while (true) {
             float currentTime = System.currentTimeMillis();
 
@@ -37,12 +78,18 @@ public class TimeStep {
 
             while (accumulator > dt) {
                 //UpdatePhysics(dt);
-                myFrame.update(dt);
+                update(dt);
                 accumulator -= dt;
-                System.out.println(dt);
+                
             }
 
-            myFrame.render();
+            BufferStrategy bs = frame.getBufferStrategy();
+			if(bs == null){
+				frame.createBufferStrategy(3);
+                return;
+			}
+			renderGraphics(bs);
         }
     }
+    
 }
